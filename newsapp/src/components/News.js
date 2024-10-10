09a28da80979b000fd47f4e487f12ec3196705df
sudho712 <sudho712@gmail.com>
2024-10-10 20:53:27 +0530

    news integrastion

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  constructor()
+  {
+    super();
+    console.log("I am constrictor")
+    this.state={
+      articles: this.articles,
+      loading: false
+    }
+  }
   render() {
+    
     return (
-      <div>
-        This is news Component of project
-        <NewsItem title="mytytle" description="mydesc"></NewsItem>
+      <div className="container my-3">
+        <h2>News Top Headlines</h2>
+     
+        <div className="row">
+        {this.state.articles.map((element)=>{
+          return 
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="mydesc" imageUrl="" newsUrl=""></NewsItem>                
+          </div>        
+ 
+        }
+
+        )}
+          
+
+          
+        </div>
+        
       </div>
     )
   }
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 
 class NewsItem extends Component {
+  
   render() {
-    let {title, description}= this.props;
+    let {title, description,imageUrl,newsUrl}=this.props;
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{ width: '18rem' }}>
-          <img src="https://via.placeholder.com/150" className="card-img-top" alt="News" />
+          <img src="imageUrl" className="card-img-top" alt="News" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
               {description}
             </p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsdetail" className="btn btn-primary">Read More</a>
           </div>
         </div>
       </div>
